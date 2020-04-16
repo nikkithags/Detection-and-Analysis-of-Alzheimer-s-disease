dd62fba23e69c18459d20cd752f35de17c6e747a
G. S. Nikkitha <48611723+nikkithags@users.noreply.github.com>
2020-04-17 00:14:05 +0530

    Create EdgeDetection.py

---
+++ EdgeDetection.py
@@ -0,0 +1,9 @@
+img = cv2.imread('brain1.jpg',0)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
